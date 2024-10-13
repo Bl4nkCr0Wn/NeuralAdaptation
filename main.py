6942abc0dd4a47b19bdbfcaf92c16d55e56f9fc1
Nevo <nevo.biton@mail.huji.ac.il>
2024-10-13 14:31:41 +0300

    Have option to use pretrained net, regularized net, or alexnet. Can commit self-supervise rotation or supervised rotation. Supervised rotation works after raising rotation epochs from 1 to 10.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 from numpy import array
 import pandas as pd
 from tensorflow.keras.models import load_model
+from sklearn.metrics import accuracy_score
 
 import preprocess
 import net
@@ -46,48 +47,38 @@ def train_new_model(data, pretrained = False):
     results = pd.DataFrame(history.history)
     return model, results
 
-
-# def self_supervised_rotate_fit(model, data, angle_range, angle_range_start = 1):
-#     # Index moving degree images by degree
-#     adaptation_generator = data.create_adaptation_generator(config.INPUT_VECTOR_SIZE)
-#     images_by_degree = preprocess.get_degree_images_dictionary(adaptation_generator)
-#
-#     # Train each image class alternatively
-#     degree_sequence = []
-#     for i in range(angle_range_start, angle_range_start + angle_range):
-#         degree_sequence.append(135 + i)
-#         degree_sequence.append((315 + i)%360)
-#
-#     for key in degree_sequence:
-#         x = images_by_degree[key]
-#         prediction = model.predict(x)[0]
-#         print('Class {} for degree {}'.format(prediction, key))
-#         y = array([prediction])
-#         model.fit(x, y, epochs=1)
-#
-#     return model
-
-def test_rotated_model(model, data):
+def test_model(model, data):
     # Test original face classes classification after rotation.
     train_generator, validation_generator, \
         test_generator = data.create_generators(config.INPUT_VECTOR_SIZE, config.BATCH_SIZE)
-    # print('Evaluate model on training data:')
-    # model.evaluate(train_generator)
+    print('Evaluate model on validation data:')
+    model.evaluate(validation_generator)
     print('Evaluate model on test data:')
     return model.evaluate(test_generator)
 
-# def self_supervision_validation_fit(model, data):
-#     train_generator, validation_generator, \
-#         test_generator = data.create_generators(config.INPUT_VECTOR_SIZE, 1)
-#
-#     for i in range(len(validation_generator)):
-#         print(f'Step {i}')
-#         x, y = validation_generator.next()
-#         prediction = model.predict(x)[0]
-#         y = array([prediction])
-#         model.fit(x, y, epochs=1)
-#
-#     return model
+def self_supervised_rotate_fit(model, data, angle_range, angle_range_start = 1):
+    # Index moving degree images by degree
+    adaptation_generator = data.create_adaptation_generator(config.INPUT_VECTOR_SIZE)
+    # images_by_degree = preprocess.get_degree_images_dictionary(adaptation_generator)
+
+    # Train each image class alternatively
+    degree_sequence = []
+    for i in range(angle_range_start, angle_range_start + angle_range):
+        degree_sequence.append(135 + i)
+        degree_sequence.append((315 + i)%360)
+
+    for i in range(0, len(degree_sequence), 2):
+        images_by_degree = preprocess.get_images_by_degree(adaptation_generator, [degree_sequence[i], degree_sequence[i+1]])
+        zipped = zip(images_by_degree[degree_sequence[i]], images_by_degree[degree_sequence[i+1]])
+        alternated_img = [item for pair in zipped for item in pair]
+        x = np.concatenate(alternated_img, axis=0)
+
+        print('Fitting {}'.format([degree_sequence[i], degree_sequence[i+1]]))
+        y = model.predict(x)
+        print('Predicted values are: {}'.format(y))
+        model.fit(x, y, epochs=10)
+
+    return model
 
 def supervised_rotate_fit(model, data, angle_range, angle_range_start = 1):
     # Index moving degree images by degree
@@ -109,40 +100,47 @@ def supervised_rotate_fit(model, data, angle_range, angle_range_start = 1):
         x = np.concatenate(alternated_img, axis=0)
         alternated_prediction = [fit, alternate_fit] * int(len(alternated_img) / 2)
         y = np.concatenate(alternated_prediction, axis=0)
-        # x = images_by_degree[degree_sequence[i]] + images_by_degree[degree_sequence[i+1]]
-        # x = np.concatenate(x, axis=0)
-        # y = ([fit,] * int(len(x)/2)) + ([alternate_fit,] * int(len(x)/2))
-        # y = np.concatenate(y, axis=0)
         print('Fitting {}'.format([degree_sequence[i], degree_sequence[i+1]]))
         print('Predicted values are: {}'.format(model.predict(x)))
-        model.fit(x, y, epochs=1)
+        model.fit(x, y, epochs=10)
 
     return model
+
+def test_model_by_class(model, data):
+    dg,vg,tg = data.create_generators(config.INPUT_VECTOR_SIZE, config.BATCH_SIZE)
+    y_pred = model.predict(tg)
+    pred = np.argmax(y_pred, axis=1)
+    test = []
+    for x, y in tg:
+        test.extend(np.argmax(y, axis=1))
+        if len(test) >= tg.samples:
+            break
+    test = np.array(test)
+    mask = test == 0
+    print('Accuracy on class 0: {}'.format(accuracy_score(test[mask], pred[mask])))
+    mask = test == 1
+    print('Accuracy on class 1: {}'.format(accuracy_score(test[mask], pred[mask])))
 
 def main():
     # data = prepare_new_data()
     data = load_data()
 
-    model, history = train_new_model(data)
+    # model, history = train_new_model(data)
     # model, history = train_new_model(data, pretrained=True)
-    model.save('alexnet_face_classifier_with_regularization.h5')
-    # model = load_model('alexnet_face_classifier_1.h5')
+    # model.save('alexnet_face_classifier_with_reg.h5')
 
-    # model = supervised_rotate_fit(model, data, 179, 1)
-    # loss, acc = test_rotated_model(model, data)
+    model = load_model('alexnet_face_classifier_no_reg.h5')
 
-    # angle_range = 15
-    # ranges = range(1, 179, angle_range)
-    # for angle in ranges:
-    #     # model = self_supervised_rotate_fit(model, data, 15, angle)
-    #     if angle + angle_range > 180:
-    #         angle_range = 180 - angle
-    #     model = supervised_rotate_fit(model, data, angle_range, angle)
-    #     loss, acc = test_rotated_model(model, data)
-    #     if acc < 0.5:
-    #         print(f'Flipping at {angle}')
-    #
-    # model.save('supervised_rotated_alexnet_face_classifier_5.h5')
+    angle_range = 15
+    ranges = range(1, 179, angle_range)
+    for angle in ranges:
+        if angle + angle_range > 180:
+            angle_range = 180 - angle
+        # model = supervised_rotate_fit(model, data, angle_range, angle)
+        model = self_supervised_rotate_fit(model, data, angle_range, angle)
+        loss, acc = test_model(model, data)
+
+    model.save('selfsupervised_rotated_alexnet_face_classifier_5_no_reg.h5')
     return
 
 if __name__ == '__main__':
--- net.py
+++ net.py
@@ -6,88 +6,99 @@ import tensorflow as tf
 class AdaptationNet(object):
     @staticmethod
     def create_model(input_shape, num_classes, loss_function, metrics):
-        model = _alex_net(input_shape, num_classes)
-
-        def l2_regularized_loss(y_true, y_pred):
-            loss = tf.keras.losses.mean_squared_error(y_true=y_true, y_pred=y_pred)
-            # regularization_loss = tf.reduce_sum(tf.square(model.trainable_weights))
-            regularization_loss = tf.add_n([ tf.nn.l2_loss(v) for v in model.trainable_weights])
-            return loss + 0.5 * regularization_loss
-
-        model.compile(optimizer=Adam(), loss=l2_regularized_loss, metrics=metrics)
-        return model
-
-    @staticmethod
-    def create_pretrained_model(input_shape, num_classes, loss_function, metrics):
-        base_model = EfficientNetB0(weights='imagenet', include_top=False, input_shape=input_shape)
-        for layer in base_model.layers:
-            layer.trainable = False
-
-        x = base_model.output
-
-        # trainable Fully Connected Layer
-        x = layers.Flatten()(x)
-        x = layers.Dense(1024, activation='relu')(x)# NOTE: compared to untrained network, here there is no dropout
-
-        # Output Layer
-        x = layers.Dense(num_classes, activation='softmax')(x)
-
-        model = models.Model(inputs=base_model.input, outputs=x)
+        model = AdaptationNet._alex_net(input_shape, num_classes)
         model.compile(optimizer=Adam(), loss=loss_function, metrics=metrics)
         return model
 
-def _alex_net(input_shape, num_classes):
-    """
-    Build ANN architecture
-    :param input_shape: (x size, y size, dimension amount)
-    :param num_classes: integer of unique classes
-    :return: model architecture
-    """
-    model = models.Sequential()
+    # @staticmethod
+    # def create_pretrained_model(input_shape, num_classes, loss_function, metrics):
+    #     base_model = EfficientNetB0(weights='imagenet', include_top=False, input_shape=input_shape)
+    #     for layer in base_model.layers:
+    #         layer.trainable = False
+    #
+    #     x = base_model.output
+    #
+    #     # trainable Fully Connected Layer
+    #     x = layers.Flatten()(x)
+    #     x = layers.Dense(1024, activation='relu')(x)# NOTE: compared to untrained network, here there is no dropout
+    #
+    #     # Output Layer
+    #     x = layers.Dense(num_classes, activation='softmax')(x)
+    #
+    #     model = models.Model(inputs=base_model.input, outputs=x)
+    #     model.compile(optimizer=Adam(), loss=loss_function, metrics=metrics)
+    #     return model
 
-    # 1st Convolutional Layer
-    model.add(layers.Conv2D(filters=96, input_shape=input_shape, kernel_size=(11, 11), strides=(4, 4), padding='valid'))
-    model.add(layers.Activation('relu'))
-    model.add(layers.BatchNormalization())
-    model.add(layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='valid'))
+    @staticmethod
+    def _alex_net(input_shape, num_classes):
+        """
+        Build ANN architecture
+        :param input_shape: (x size, y size, dimension amount)
+        :param num_classes: integer of unique classes
+        :return: model architecture
+        """
+        model = models.Sequential()
 
-    # 2nd Convolutional Layer
-    model.add(layers.Conv2D(filters=256, kernel_size=(5, 5), strides=(1, 1), padding='same'))
-    model.add(layers.Activation('relu'))
-    model.add(layers.BatchNormalization())
-    model.add(layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='valid'))
+        # 1st Convolutional Layer
+        model.add(
+            layers.Conv2D(filters=96, input_shape=input_shape, kernel_size=(11, 11), strides=(4, 4), padding='valid'))
+        model.add(layers.Activation('relu'))
+        model.add(layers.BatchNormalization())
+        model.add(layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='valid'))
 
-    # 3rd Convolutional Layer
-    model.add(layers.Conv2D(filters=384, kernel_size=(3, 3), strides=(1, 1), padding='same'))
-    model.add(layers.Activation('relu'))
-    model.add(layers.BatchNormalization())
+        # 2nd Convolutional Layer
+        model.add(layers.Conv2D(filters=256, kernel_size=(5, 5), strides=(1, 1), padding='same'))
+        model.add(layers.Activation('relu'))
+        model.add(layers.BatchNormalization())
+        model.add(layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='valid'))
 
-    # 4th Convolutional Layer
-    model.add(layers.Conv2D(filters=384, kernel_size=(3, 3), strides=(1, 1), padding='same'))
-    model.add(layers.Activation('relu'))
-    model.add(layers.BatchNormalization())
+        # 3rd Convolutional Layer
+        model.add(layers.Conv2D(filters=384, kernel_size=(3, 3), strides=(1, 1), padding='same'))
+        model.add(layers.Activation('relu'))
+        model.add(layers.BatchNormalization())
 
-    # 5th Convolutional Layer
-    model.add(layers.Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding='same'))
-    model.add(layers.Activation('relu'))
-    model.add(layers.BatchNormalization())
-    model.add(layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='valid'))
+        # 4th Convolutional Layer
+        model.add(layers.Conv2D(filters=384, kernel_size=(3, 3), strides=(1, 1), padding='same'))
+        model.add(layers.Activation('relu'))
+        model.add(layers.BatchNormalization())
 
-    # Flatten
-    model.add(layers.Flatten())
+        # 5th Convolutional Layer
+        model.add(layers.Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding='same'))
+        model.add(layers.Activation('relu'))
+        model.add(layers.BatchNormalization())
+        model.add(layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='valid'))
 
-    # 1st Fully Connected Layer
-    model.add(layers.Dense(4096))
-    model.add(layers.Activation('relu'))
-    model.add(layers.Dropout(0.5))
+        # Flatten
+        model.add(layers.Flatten())
 
-    # 2nd Fully Connected Layer
-    model.add(layers.Dense(4096))
-    model.add(layers.Activation('relu'))
-    model.add(layers.Dropout(0.5))
+        # 1st Fully Connected Layer
+        model.add(layers.Dense(4096))
+        model.add(layers.Activation('relu'))
+        model.add(layers.Dropout(0.5))
 
-    # Output Layer
-    model.add(layers.Dense(num_classes))
-    model.add(layers.Activation('softmax'))
+        # 2nd Fully Connected Layer
+        model.add(layers.Dense(4096))
+        model.add(layers.Activation('relu'))
+        model.add(layers.Dropout(0.5))
 
-    return model
+        # Output Layer
+        model.add(layers.Dense(num_classes))
+        model.add(layers.Activation('softmax'))
+
+        return model
+
+# class RotationNet(object):
+#     @staticmethod
+#     def convert_model(model):
+#         new = tf.keras.models.clone_model(model)
+#         for layer in new.layers[:-2]:
+#             layer.trainable = False
+#
+#         def l2_regularized_loss(y_true, y_pred):
+#             loss = tf.keras.losses.mean_squared_error(y_true=y_true, y_pred=y_pred)
+#             regularization_loss = tf.add_n([ tf.nn.l2_loss(v) for v in model.trainable_weights]) # doesnt converge
+#             # regularization_loss = tf.add_n([ tf.nn.l2_loss(v) for v in model.layers[-2].weights])# didnt try yet
+#             return loss + regularization_loss
+#
+#         new.compile(optimizer=Adam(), loss=l2_regularized_loss, metrics=model.metrics)
+#         return new
